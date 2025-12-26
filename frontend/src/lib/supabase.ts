476d8280a90b9372bfcdf1cb08e53229187efc44
Dianana101 <irmarude1984@gmail.com>
2025-12-27 02:55:20 +0300

    feat(tst): add styles

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -4,3 +4,19 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log(import.meta.env);
+// Для локальной разработки - установить тестовую сессию
+if (import.meta.env.DEV) {
+  supabase.auth.setSession({
+    access_token: 'fake_dev_token',
+    refresh_token: 'fake_dev_token',
+    user: {
+      id: 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', // ID тестового пользователя
+      email: 'test@example.com',
+      app_metadata: {},
+      user_metadata: {},
+      aud: 'authenticated',
+      created_at: new Date().toISOString()
+    }
+  } as any);
+}
